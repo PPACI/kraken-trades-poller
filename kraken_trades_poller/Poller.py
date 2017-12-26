1ad61a4dc8f246e37844f767b9a5aa6aa566f0ba
pierre paci <pierre.paci@gmail.com>
2017-12-26 10:24:19 +0100

    main loop

--- kraken_trades_poller/Poller.py
+++ kraken_trades_poller/Poller.py
@@ -3,6 +3,8 @@ from datetime import date
 from typing import List, Optional, Counter
 
 import ccxt
+import elasticsearch.helpers
+from ccxt import ExchangeError
 from elasticsearch_dsl.connections import connections
 
 from kraken_trades_poller.Trade import Trade
@@ -16,26 +18,35 @@ class Poller(object):
         connections.create_connection(hosts=es_host)
         init_index(es_index=es_index)
         self.last_transaction = {}
+        self.doc_count = Counter()
 
     def start_loop(self):
         kraken = ccxt.kraken()
         while True:
+            bulk_docs = []
             trade_counter = Counter()
             kraken.load_markets()
-            print([symbol for symbol in kraken.markets])
-            for symbol in kraken.markets:
+            eur_usd_market = [symbol for symbol in kraken.markets if 'EUR' in symbol or 'USD' in symbol]
+            print(eur_usd_market)
+            for symbol in eur_usd_market:
                 time.sleep(kraken.rateLimit / 1000)  # time.sleep wants seconds
-                for trade in kraken.fetch_trades(symbol, since=self.get_last_transaction(symbol=symbol)):
-                    es_trade = Trade(pair=symbol,
-                                     price=trade['price'],
-                                     timestamp_transaction=trade['datetime'],
-                                     volume=trade['amount'])
-                    es_trade.save()
-                    trade_counter.update([symbol])
-                    self.update_last_transaction(timestamp=trade['timestamp'], symbol=symbol)
-                print('{} Done'.format(symbol))
+                try:
+                    for trade in kraken.fetch_trades(symbol, since=self.get_last_transaction(symbol=symbol)):
+                        es_trade = Trade(pair=symbol,
+                                         price=trade['price'],
+                                         timestamp_transaction=trade['datetime'],
+                                         volume=trade['amount'])
+                        bulk_docs.append(es_trade.to_dict(include_meta=True))
+                        trade_counter.update([symbol])
+                        self.update_last_transaction(timestamp=trade['timestamp'], symbol=symbol)
+                    print('{} Done'.format(symbol))
+                except ExchangeError as e:
+                    print("problem with {}".format(symbol))
+                    continue
+            print('Now Indexing')
+            elasticsearch.helpers.bulk(client=connections.get_connection(), actions=bulk_docs, max_retries=3)
             print(trade_counter)
-            time.sleep(60*5)
+            time.sleep(60 * 5)
 
     def update_last_transaction(self, timestamp: int, symbol: str):
         if symbol in self.last_transaction:
@@ -49,5 +60,3 @@ class Poller(object):
             return self.last_transaction[symbol]
         else:
             return None
-
-
